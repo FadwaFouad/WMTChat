c221bb8c94b92d408381cd973b369c0e2814ddb9
FadwaFouad <fadoalqasem@gmail.com>
2022-09-05 09:04:33 +0200

    add new broadcast activity change ic_launcher

---
+++ app/src/main/java/com/tech4dev/wmtchat/NewBroadcastActivity.kt
@@ -0,0 +1,16 @@
+package com.tech4dev.wmtchat
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class NewBroadcastActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_new_broadcast)
+
+    }
+
+}
+
+
+
