9114f50ca86b1ba938839efe56e914f9e2742b56
FadwaFouad <fadoalqasem@gmail.com>
2022-09-06 19:12:16 +0200

    change Theme-Add chat activity

--- app/src/main/java/com/tech4dev/wmtchat/NewGroupActivity.kt
+++ app/src/main/java/com/tech4dev/wmtchat/NewGroupActivity.kt
@@ -2,6 +2,7 @@ package com.tech4dev.wmtchat
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.widget.Toolbar
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -9,11 +10,19 @@ import com.tech4dev.wmtchat.adapter.ListOfFriendsAdapter
 
 class NewGroupActivity : AppCompatActivity() {
     private lateinit var recyclerView: RecyclerView
+    private lateinit var toolbar: Toolbar
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_new_group)
+        toolbar=findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
+        initRecyclerView()
+    }
+
+    private fun initRecyclerView() {
         recyclerView = findViewById(R.id.listOfFriends)
         recyclerView.adapter = ListOfFriendsAdapter(this)
         recyclerView.layoutManager = LinearLayoutManager(this)
